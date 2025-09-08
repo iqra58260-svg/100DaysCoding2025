bf4f5c82e8f4bb624af7a37f93e28b83bbc3e74b
iqra58260-svg <iqra58260@gmail.com>
2025-09-08 12:50:07 +0800

    Create Days001.java
    Pada hari pertama saya membuat program "Hello World" dan struktur dasar kode java

---
+++ Days001.java
@@ -0,0 +1,16 @@
+*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.days1;
+
+/**
+ *
+ * @author Iqra
+ */
+public class Days1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
