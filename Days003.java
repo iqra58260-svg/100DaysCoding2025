0b2cd3f7827c5757a0c6616d789e910c88f62aed
iqra58260-svg <iqra58260@gmail.com>
2025-09-10 11:15:31 +0800

    Create Days003.java
    Memahami dan Menggunakan Komentar

---
+++ Days003.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.days3;
+
+/**
+ *
+ * @author Iqra
+ */
+public class Days3 {
+
+    public static void main(String[] args) {
+//        System.out.println("Memahami dan Menggunakan Komentar");
+//        Komentar catatan penjelasan singkat yang ditambahkan ke kode untuk keuntungan dari mereka yang membacanya
+    }
+}
