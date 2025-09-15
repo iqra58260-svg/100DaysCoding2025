14d39be01e918901945efc96ea5940579ba3829b
iqra58260-svg <iqra58260@gmail.com>
2025-09-15 10:53:55 +0800

    Create Days008.Java

---
+++ Days008.Java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package days8;
+
+/**
+ *
+ * @author Iqra
+ */
+public class Days8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+String nama = "Iqra";
+String prodi = "Informatika";
+String fakultas = "Teknik";
+String kampus = "Universitas Sulawesi Barat";
+        System.out.println("Nama:"+ nama);
+        System.out.println("Program Studi:"+ prodi);
+        System.out.println("Fakultas:"+ fakultas);
+        System.out.println("Kampus:"+ kampus);
+    }
+    
+}
